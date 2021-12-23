00cb9a0eb102a195d882a3abbf4921c4e15cf384
dvargas42 <dvargas.eng@gmail.com>
2021-12-23 00:49:53 -0300

    feat: implement component body

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,40 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard"
+
+import '../styles/content.scss';
+interface ContentProps {
+  genreTitle: string;
+  moviesList: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Value: string;
+    }>
+    Runtime: string;
+  }>
 }
+
+function Content({ genreTitle, moviesList }: ContentProps) {
+ return (
+  <div className="container">
+    <header>
+      <span className="category">Categoria:<span>{genreTitle}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {moviesList.map(movie => (
+          <MovieCard 
+            key ={movie.imdbID}
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  </div>
+ )
+}
+ export { Content }
